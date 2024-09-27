cf869b61918f673bce5103dd9848d980d1370bbe
sajishglbajaj <sajishkumarchakia306@gmail.com>
2024-09-27 11:52:24 +0530

    Add files via upload

---
+++ compound.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+float p=2,r=3,t=1,n=1;
+float c=p*(pow(1+(r/100),t));
+printf("%f \n",c);
+
+}
+
---
+++ swap1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a=2,b=3,c;
+c=a;
+a=b;
+b=c;
+printf("%d \n",a);
+printf("%d",b);
+}
+
