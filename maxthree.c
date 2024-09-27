5cf6ae562151a359f4d8b18d0b3648a1b567994d
sajishglbajaj <sajishkumarchakia306@gmail.com>
2024-09-27 12:02:14 +0530

    Add files via upload

---
+++ maxthree.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int a=2,b=3,c=4,max;
+max=a;
+if(b>=max){
+	max=b;
+}
+if(c>=max){
+	max=c;
+}
+printf("%d \n",max);
+;
+}
+
---
+++ maxthree1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int a=2,b=3,c=4;
+if(a>=b &&a>=c){
+	printf("%d",a);
+}
+if(b>=a && b>=c){
+	printf("%d",b);
+}
+if(c>=a && c>=b){
+	printf("%d",c);
+}
+;
+}
+
