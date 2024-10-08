46804af42d5e219b0e3c1aba7899e2f186fbb496
sajishglbajaj <sajishkumarchakia306@gmail.com>
2024-10-08 16:27:08 +0530

    Add files via upload

---
+++ star3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n, i, j, a = 1;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+
+            printf("%d ", a);
+            a = a + 1;
+        }
+        printf("\n");
+    }
+    return 0;
+}
